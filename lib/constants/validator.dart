c0a0ae2d04bfad63088d1f38f5f29a82342b371c
NayemSoftvence <nayem.softvence@gmail.com>
2025-07-04 02:11:38 +0600

    update

--- lib/constants/validator.dart
+++ lib/constants/validator.dart
@@ -19,6 +19,17 @@ String? passwordValidator(String? value) {
   return null;
 }
 
+// Confirm Password Validator
+String? confirmPasswordValidator(String? value, String? originalPassword) {
+  if (value == null || value.isEmpty) {
+    return 'Please confirm your password';
+  }
+  if (value != originalPassword) {
+    return 'Passwords do not match';
+  }
+  return null;
+}
+
 String? validatePhoneNumber(String? value) {
   // Regular expression for validating international phone numbers
   // String pattern = r'^\+?([0-9]{1,3})?[-.●\s]?([0-9]{1,4})[-.●\s]?([0-9]{1,4})[-.●\s]?([0-9]{1,9})$';
@@ -30,5 +41,22 @@ String? validatePhoneNumber(String? value) {
   // else if (!regExp.hasMatch(value)) {
   //   return 'Please enter a valid phone number';
   // }
+  return null;
+}
+
+//validation Name
+String? nameValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Name is required';
+  }
+
+  return null;
+}
+
+//validation Name
+String? messageValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Enter your Message';
+  }
   return null;
 }
